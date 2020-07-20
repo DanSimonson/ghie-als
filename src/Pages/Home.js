13df8b6f42cbf306584b3629d6dae985992b4f1b
DanSimonson <dansimonson@mariposaweb.net>
2020-07-20 00:57:21 -0600

    added first draft of responsive navbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import "./Home.scss";
 import { AlsContext } from "../Context/AlsContext";
+import Navbar from "../Nav/Navbar";
 
 function Home() {
   const { myValue, setMyValue } = useContext(AlsContext);
 
   return (
     <div className="home">
+      <Navbar />
       <h1>{myValue}</h1>
     </div>
   );
