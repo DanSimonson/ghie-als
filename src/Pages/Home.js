d9420bb7aee686e55044cff03c91cae501ba4ae4
DanSimonson <dansimonson@mariposaweb.net>
2020-07-21 22:09:59 -0600

    added text classes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,15 +12,17 @@ function Home() {
       <Navbar />
 
       <Carousel>
-        <Carousel.Item>
+        <Carousel.Item className="text-secondary">
           <img
             className="d-block w-100"
             src="https://res.cloudinary.com/dmglopmul/image/upload/v1595356919/ghie-als/iloveals.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3 className="text-dark">First slide label</h3>
+            <p className="text-dark">
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -31,8 +33,10 @@ function Home() {
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3 className="text-dark">Second slide label</h3>
+            <p className="text-dark">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -43,8 +47,8 @@ function Home() {
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
+            <h3 className="text-dark">Third slide label</h3>
+            <p className="text-dark">
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
           </Carousel.Caption>
